a0d6c5aab4000e498cc762e94cc697bb12e4147b
abhishekgupta01 <abhishekgupta@gofynd.com>
2025-06-03 07:45:17 +0530

    added side and main bar

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,18 +1,21 @@
 import { useState } from "react";
 import "./App.css";
 import TicTakToe from "./components/TicTacToe";
+import { Link, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className="main-container">
+        <div className="side-bar">
+          <Link to={"/tic-tac-toe"}>Tic Tac Toe</Link>
+        </div>
+        <div className="content">
+          <Routes>
+            <Route path="/tic-tac-toe" element={<TicTakToe />} />
+          </Routes>
+        </div>
       </div>
-      <TicTakToe />
     </>
   );
 }
