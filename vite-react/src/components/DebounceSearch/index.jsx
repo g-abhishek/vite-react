5193c43774aaa72233aadd0f434028b8925b2591
abhishekgupta01 <abhishekgupta@gofynd.com>
2025-06-14 15:47:23 +0530

    debounce hook that will only return debounced value

--- vite-react/src/components/DebounceSearch/index.jsx
+++ vite-react/src/components/DebounceSearch/index.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { debounce } from "../../utils";
+import useDebounce from "../../hooks/useDebounce";
 
 const DebounceSearch = () => {
   const [search, setSearch] = useState("");
@@ -7,10 +8,21 @@ const DebounceSearch = () => {
     setSearch(text);
   }, 300);
 
+  // use debounce hook that will only return debounced value
+  const [query, setQuery] = useState("");
+  const [debouncedValue, setDebouncedValue] = useState("");
+  const debouncedQuery = useDebounce(query, 500);
+  useEffect(() => {
+    if (debouncedQuery) {
+      setDebouncedValue(debouncedQuery);
+    }
+  }, [debouncedQuery]);
+
   return (
     <>
       <div>DebounceSearch</div>
       <div>
+        <p>Utility Function</p>
         <input
           type="text"
           onChange={(e) => {
@@ -18,6 +30,19 @@ const DebounceSearch = () => {
           }}
         />
         <p>Search Text: {search}</p>
+      </div>
+
+      <br />
+      <br />
+      <div>
+        <p>useDebounceHook: this hook will only return debounced value</p>
+        <input
+          type="text"
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
+        />
+        <p>Search Text: {debouncedValue}</p>
       </div>
     </>
   );
