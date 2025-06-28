29d21cd1ec3bf24a8e146b67c3438799332635b9
abhishekgupta01 <abhishekgupta@gofynd.com>
2025-06-29 00:51:24 +0530

    reserved, custom events, broadcasting

--- node-socket/index.js
+++ node-socket/index.js
@@ -12,11 +12,24 @@ httpServer.listen(3000, () => {
 });
 
 // Socket.io Connection
+let users = 0;
+
 io.on("connection", (socket) => {
-    console.log(socket);
   console.log("A user connected");
+  users++;
+
+  // Send a message to the connected client
+  // pre-reserved events, client to listen on "message" event
+  socket.send("Hello from server!");
+
+  // Broadcast to all other clients (excluding the sender)
+  socket.broadcast.emit(
+    "announcement",
+    `New user connected. Total count is ${users}`
+  );
 
   socket.on("disconnect", () => {
+    users--;
     console.log("User disconnected");
   });
 });
